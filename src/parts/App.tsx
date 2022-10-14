1d2c0bc40797b0a0f35cc3e0ea620199b909c79e
JT Broad <jack101091@gmail.com>
2022-10-14 20:51:02 +0000

    Update 3 files

--- src/parts/App.tsx
+++ src/parts/App.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { signal } from '@preact/signals-react';
-import { Button } from '../components/react/Button';
-import { CountStore } from '../models/CountStore';
+import React from "react";
+import { Button } from "../components/react/Button";
+import { CountStore } from "../models/CountStore";
 
 const store = new CountStore();
 
-export function App(){
-    return (
-        <div className='w-full h-full p-4 absolute top-0 left-0' data-theme='business'>
-            <div className='text-4xl'>Hello there General Kenobi.</div>
-            <div className='py-4 text-3xl'>
-                { store.count }
-            </div>
-            <Button onClick={()=>{
-                store.increment()
-            }}>
-                Click Here To Increment The Count
-            </Button>
-        </div>
-    )
+export function App() {
+  return (
+    <div
+      className="w-full h-full p-4 absolute top-0 left-0"
+      data-theme="business"
+    >
+      <div className="text-4xl">Hello there General Kenobi.</div>
+      <div className="py-4 text-3xl">{store.count}</div>
+      <Button
+        onClick={() => {
+          store.increment();
+        }}
+      >
+        Click Here To Increment The Count
+      </Button>
+    </div>
+  );
 }
