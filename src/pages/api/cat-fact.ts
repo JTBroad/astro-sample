f76f977e4271a4b7930d35799a37473e03870bb4
JT Broad <Jack101091@gmail.com>
2022-11-04 22:19:09 -0400

    added example api get call.

---
+++ src/pages/api/cat-fact.ts
@@ -0,0 +1,16 @@
+
+export async function get({ params }) {
+
+    const response = await fetch("https://catfact.ninja/fact");
+    const data = await response.json();
+
+    return new Response(JSON.stringify({
+        CATFACT:data.fact
+    }), 
+    {
+        status:200,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+}
