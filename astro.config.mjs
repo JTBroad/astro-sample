eefc721f0ee122ebc7d33f608730ffe7460e912d
JT Broad <Jack101091@gmail.com>
2022-11-04 21:10:37 -0400

    initial ssr enabling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import windicss from 'astro-windicss';
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [windicss(), react()]
+  integrations: [windicss(), react()],
+  output: "server",
+  adapter: node({
+    mode:'standalone'
+  })
 });
