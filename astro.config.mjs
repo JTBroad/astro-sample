adf61afd4af12a003506cb6a05f5a02e0dd49ef0
JT Broad <jack101091@gmail.com>
2022-11-20 16:37:28 -0500

    adding service worker back

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [windicss(), react()]
+  integrations: [windicss(), react(), serviceWorker()]
 });
