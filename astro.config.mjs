bb92fcbe7fbe29a6b1e3644c846d53741344abda
JT Broad <jack101091@gmail.com>
2022-11-20 22:08:32 -0500

    adding service worker integration to test with ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import serviceWorker from "astrojs-service-worker";
 import windicss from 'astro-windicss';
 
 // https://astro.build/config
@@ -9,7 +10,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [windicss(), react()],
+  integrations: [windicss(), react(), serviceWorker()],
   output: "server",
   server: (command) => ({ port: command === 'dev' ? 3000 : 80, host: command === 'dev' ? false : '0.0.0.0' }),
   adapter: node({ 
