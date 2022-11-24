0238ed9b1a62f5a07e8b1b0106219ba4fe894b9b
JT Broad <Jack101091@gmail.com>
2022-11-23 22:42:37 -0500

    got a build working woo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+// import type { AstroConfig, AstroIntegration, AstroRenderer } from 'astro';
 import { defineConfig } from 'astro/config';
 import windicss from 'astro-windicss';
 
@@ -6,12 +7,53 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 import node from "@astrojs/node";
+// import cloudflare from "@astrojs/cloudflare";
+function MyInt(config) {
+  let _staticBuild = false;
+  return {
+    name: '@astrojs/solid-js',
+    hooks: {
+      'astro:config:setup': async options => {
+        console.log('1', options);
+        if(options.config.output === 'static'){
+          _staticBuild = true;
+        }
+      },
+      'astro:build:start': async options => {
+        console.log('2', options);
+      },
+      'astro:build:setup': async options => {
+        // delete options.pages['src/pages/api/cat-facts.ts'];
+        if(!options.vite.build.ssr && _staticBuild){
+          for(let key of options.pages.keys()){
+            // if()
+            if(config.staticRoutes.indexOf(key) === -1){
+              options.pages.delete(key);
+            }
+          }
+          // options.pages.delete('src/pages/api/cat-facts.ts');
+          // options.pages.delete('src/pages/index.astro');
+          // options.pages = undefined;
+          // console.log(options, config);
+        }
+        // addRenderer(getRenderer());
+        // updateConfig({ vite: await getViteConfiguration(command === 'dev', config) });
+      }
+    }
+  };
+}
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [windicss(), react()],
+  integrations: [windicss(), react(), MyInt({
+    staticRoutes: [
+      // 'src/pages/app/index.astro'
+    ]
+  })],
   output: "server",
-  server: (command) => ({ port: command === 'dev' ? 3000 : 80, host: command === 'dev' ? false : '0.0.0.0' }),
+  server: (command) => ({ port: command === 'dev' ? 3000 : 3080, host: command === 'dev' ? false : '127.0.0.1' }),
   adapter: node({ 
     mode:'standalone'
   })
---
+++ src/pages/api/cat-facts.ts
@@ -0,0 +1,19 @@
+
+export const Bob = 2;
+
+// export async function get({ params }) {
+
+//     const response = await fetch("https://catfact.ninja/fact");
+//     const data = await response.json();
+
+//     return { bob: 'steve' }
+//     // return new Response(JSON.stringify({
+//     //     CATFACT:data.fact
+//     // }), 
+//     // {
+//     //     status:200,
+//     //     headers: {
+//     //         "Content-Type": "application/json"
+//     //     }
+//     // })
+// }
